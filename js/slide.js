f7f642243c0b0d575eb51c617065226e9de21f2b
cch <aehtla2012@gmail.com>
2023-07-06 23:49:54 +0800

    [IMP] hydrangeas_ch done

---
+++ js/slide.js
@@ -0,0 +1,60 @@
+window.onload = function () {
+    let intervalId;
+    
+    intervalId = setInterval(function () {
+        plusSlides();
+    }, 5000);
+
+    let slideIndex = 1;
+    showSlide(slideIndex);
+  
+    let prev = document.getElementById("prev");
+    prev.addEventListener("click", divideSlides, false);
+  
+    let next = document.getElementById("next");
+    next.addEventListener("click", plusSlides, false);
+  
+    const selectdot = document.querySelectorAll(".dot");
+    for (let i = 0; i < selectdot.length; i++) {
+      selectdot[i].addEventListener("click", function (e) {
+        showSlide((slideIndex = i + 1));
+      });
+    }
+
+    function plusSlides() {
+      showSlide((slideIndex += 1));
+      clearInterval(intervalId);
+      intervalId = setInterval(function () {
+        plusSlides();
+      }, 5000);
+    }
+  
+    function divideSlides() {
+      showSlide((slideIndex -= 1));
+      clearInterval(intervalId);
+      intervalId = setInterval(function () {
+        plusSlides();
+      }, 5000);
+    }
+  
+    function showSlide(num) {
+      let slides = document.getElementsByClassName("slide__item");
+      let dots = document.getElementsByClassName("dot");
+      if (num > slides.length) {
+        slideIndex = 1;
+      }
+  
+      if (num < 1) {
+        slideIndex = slides.length;
+      }
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+      }
+  
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+    }
+  };
