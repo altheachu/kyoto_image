2cef76437eada2ff236af91288344343e2630dde
cch <aehtla2012@gmail.com>
2023-09-17 13:09:04 +0800

    [IMP] food content 100%

--- js/food.js
+++ js/food.js
@@ -35,23 +35,37 @@ const store = [
     },
     {
         name: "いづ重 壽司",
-        intro: "八阪神社對面的老店，以傳承百年的鯖姿壽司及壓壽司聞名",
-        website: '<a href="https://gion-izuju.com/" target="_blank">https://gion-izuju.com/</a>'
+        intro: "衹園八阪神社對面的老店，以傳承百年的鯖姿壽司及壓壽司聞名",
+        website: '<a href="https://gion-izuju.com/" target="_blank">https://gion-izuju.com/</a>',
+        comments: '京都位於不臨海的盆地，因此外地的漁獲需要經過鹽漬方能保存，因此發展出鯖姿壽司這項特色美食。' + 
+        '鯖姿壽司是以昆布包裹鯖魚與白飯捲起再切片，品嘗時要魚肉連白飯一起咀嚼，才能體會兩者相得益彰的滋味。' +
+        '此外，這家店的箱壽司與啤酒也相當美味。這家店採用梯次出餐的方式，出餐時也會把同桌的餐點併成一盤，這種翻桌方式也讓我大開眼界。'
     },
     {
         name: "中村藤吉",
         intro: "位於宇治的百年茶商，擁有日式、西式各種美味的抹茶甜品",
-        website: '<a href="https://tokichi.jp/" target="_blank">https://tokichi.jp/</a>'
+        website: '<a href="https://tokichi.jp/" target="_blank">https://tokichi.jp/</a>',
+        comments: '這家店雖然以販售抹茶茶葉起家，卻漸漸發展出各式高品質的抹茶食品與甜點。' + 
+        '抹茶蕎麥麵口味其實一般，上面的豆皮倒是不錯。' + 
+        '抹茶聖代的最上層是抹茶冰淇淋及白玉糰子，下層則包含抹茶蛋糕和抹茶果凍，每個部分都用甜味平衡了抹茶的苦澀，飄散著淡淡的抹茶香氣，真是絕品！' + 
+        '招牌抹茶巴菲除了上述內料外，還多加了一層堅果增添口感，最上層則用抹茶粉畫出了中村藤吉的Logo，十分吸睛。',
     },
     {
         name: "宇治川餅",
         intro: "古法手作抹茶糰子，擁有濃厚茶香與獨特口感",
-        website: '<a href="https://www.facebook.com/ujigawamochiootu/" target="_blank">https://www.facebook.com/ujigawamochiootu/</a>'
+        website: '<a href="https://www.facebook.com/ujigawamochiootu/" target="_blank">https://www.facebook.com/ujigawamochiootu/</a>',
+        comments: '在欣賞完三戶室寺盛開的繡球花後，在出口的攤販試吃抹茶糰子，一開始不抱任何期待，放入口中咀嚼一番後驚為天人！' + 
+        '雖然抹茶的味道很重，卻不會感到任何苦澀，因為糰子的甜度跟抹茶調和地非常好。甜而不膩的滋味，配合充滿嚼勁的口感，真是意外的驚喜。' + 
+        '因為實在美味，所以立刻購買當作給同事的土產，受到大家的好評呢！' + 
+        '這種小零食的好處，除了可以讓自己開心之外，還可以跟別人分享後一起討論感想，這也是種樂趣！',
     },
     {
         name: "YAYOI 彌生軒",
         intro: "平價和洋定食的專賣店，採用自動點餐機、自動加飯機等新奇的設備",
-        website: '<a href="https://www.yayoi.com.tw/jp/index.php" target="_blank">https://www.yayoi.com.tw/jp/index.php</a>'
+        website: '<a href="https://www.yayoi.com.tw/jp/index.php" target="_blank">https://www.yayoi.com.tw/jp/index.php</a>',
+        comments: '彌生軒是專賣定食的平價連鎖餐廳，在台灣也有分店，但是我覺得在台灣的市場定位偏輕奢華，但是京都的是家常料理，'+
+        '像是我們這次點的唐揚炸雞定食及辣湯麵都是台灣沒有的品項：唐揚雞外脆內多汁，真的好好吃！定時內含的味噌湯也濃郁美味。' + 
+        '辣湯麵的湯頭雖然辛辣卻又十分滑潤，會激起滿滿的食欲。另外這家店有個很厲害的「自動加飯機」，我是第一次見到這種機器！',
     },
 ]
 
@@ -111,6 +125,7 @@ document.getElementById('store_5').addEventListener("click", function(){
     document.getElementById('store_name').innerHTML = store[4].name;
     document.getElementById('store_intro').innerHTML = store[4].intro;
     document.getElementById('store_website').innerHTML = store[4].website;
+    document.getElementById('comments').innerHTML = store[4].comments;
     
     if (Number(window.screen.availWidth) < 821){
         document.getElementById('modal_container').style.display="inline-block";
@@ -122,6 +137,7 @@ document.getElementById('store_6').addEventListener("click", function(){
     document.getElementById('store_name').innerHTML = store[5].name;
     document.getElementById('store_intro').innerHTML = store[5].intro;
     document.getElementById('store_website').innerHTML = store[5].website;
+    document.getElementById('comments').innerHTML = store[5].comments;
     
     if (Number(window.screen.availWidth) < 821){
         document.getElementById('modal_container').style.display="inline-block";
@@ -133,6 +149,7 @@ document.getElementById('store_7').addEventListener("click", function(){
     document.getElementById('store_name').innerHTML = store[6].name;
     document.getElementById('store_intro').innerHTML = store[6].intro;
     document.getElementById('store_website').innerHTML = store[6].website;
+    document.getElementById('comments').innerHTML = store[6].comments;
     
     if (Number(window.screen.availWidth) < 821){
         document.getElementById('modal_container').style.display="inline-block";
@@ -144,6 +161,7 @@ document.getElementById('store_8').addEventListener("click", function(){
     document.getElementById('store_name').innerHTML = store[7].name;
     document.getElementById('store_intro').innerHTML = store[7].intro;
     document.getElementById('store_website').innerHTML = store[7].website;
+    document.getElementById('comments').innerHTML = store[7].comments;
     
     if (Number(window.screen.availWidth) < 821){
         document.getElementById('modal_container').style.display="inline-block";
